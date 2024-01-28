e5eebabad837e2ea5b2d9ed980ef2bb5f9444aac
Gazi Jakia Sultanta <107138150+jakia12@users.noreply.github.com>
2024-01-29 01:38:30 +0600

    Add files via upload
    updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
   const [menu, setMenu] = useState(false);
 
   return (
-    <div className="fixed w-[97vw] right-0 left-0 mx-auto z-10">
+    <div className="fixed lg:w-[97vw] w-full right-0 left-0 mx-auto z-10 lg:bg-transparent bg-[#0000006b]">
       <div className="flex justify-between items-center lg:border-b lg:border-[#989898] mx-5 lg:mx-10 py-6">
         <Link to="/" className="text-3xl logo_txt">
           ODIN ✦
@@ -34,7 +34,7 @@ const Navbar = () => {
           </li>
         </ul>
         {menu && (
-          <ul className="bg-[#0e0d0dcb] absolute right-10 top-14 p-4 rounded-md shadow-lg block lg:hidden">
+          <ul className="bg-[#000000cb] absolute right-10 top-14 p-4 rounded-md shadow-lg block lg:hidden">
             <li>
               <Link to="#" className="text-[#989898] text-[18px] font-[500]">
                 Product
